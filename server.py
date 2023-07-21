83b0ca84c5022a4a3e16683f4e7dc6145e706178
Green Gecko <34012548+greengeckowizard@users.noreply.github.com>
2023-07-21 17:15:18 -0500

    Update server.py

--- server.py
+++ server.py
@@ -41,7 +41,7 @@ app.add_middleware(
 )
 
 MODEL_MAPPING = {
-    "sage": "capybara",
+    "Assistant": "capybara",
     "claude-instant": "a2",
     "claude-2-100k": "a2_2",
     "claude-instant-100k": "a2_100k",
